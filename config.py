ec0c9c539a6f767bfd0f64ea831d8ac58dd286d3
jason-kywong <18948289663@163.com>
2023-06-04 16:56:37 +0800

    Initial commit

---
+++ config.py
@@ -0,0 +1,42 @@
+# coding = utf-8
+import os
+
+DEBUG = True
+
+SECRET_KEY = os.urandom(24)
+MAX_CONTENT_LENGTH = 16 * 1024 * 1024
+
+# mysql配置
+DIALECT = "mysql"
+DRIVER = "pymysql"
+USERNAME = "root"
+PASSWORD = "Aa123456"
+HOST = "localhost"
+PORT = "3307"
+DATABASE = "new_shop"
+
+SQLALCHEMY_DATABASE_URI = "{}+{}://{}:{}@{}:{}/{}?charset=utf8".format(DIALECT, DRIVER, USERNAME, PASSWORD, HOST, PORT,
+                                                                       DATABASE)
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# qq邮箱配置
+
+MAIL_SERVER = 'smtp.163.com'
+MAIL_PROT = 25
+MAIL_USE_TLS = True
+MAIL_USE_SSL = False
+MAIL_USERNAME = "18948289663@163.com"
+MAIL_DEFAULT_SENDER = "18948289663@163.com"
+MAIL_PASSWORD = "ADNJGBPMWOGYOFKI"
+MAIL_DEBUG = True
+
+# redis配置
+REDIS_HOST = "localhost"
+REDIS_PORT = 6379
+
+# 七牛云配置
+ALLOWED_EXT=set(['png', 'jpg','jpeg','bmp','gif'])
+QINIU_ACCESS_KEY = "AaPT2jsEfuGggQFChS4I-IVTgO55ai0PARJL0gBC"
+QINIU_SECRET_KEY = "UxgbGBaIqPMJXAkjWUhdUu9xeXTCRoSFENdAEFLy"
+QINIU_BUCKET_NAME = 'download'
+QINIU_URL = "rvpkzi39r.hd-bkt.clouddn.com"
